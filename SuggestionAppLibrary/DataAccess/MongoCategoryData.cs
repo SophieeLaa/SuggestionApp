aa66e98be0d94e752199b1faac735c3c6a01f9ad
sophie 888 <sophie88998899@gmail.com>
2024-03-15 17:49:05 +0400

    Adding Category and Status

--- SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
+++ SuggestionAppLibrary/DataAccess/MongoCategoryData.cs
@@ -7,11 +7,33 @@ using System.Threading.Tasks;
 
 namespace SuggestionAppLibrary.DataAccess
 {
-    public class MongoCategoryData
+    public class MongoCategoryData : ICategoryData
     {
-        public MongoCategoryData(IDbConnection db,IMemoryCache memoryCache)
+        private readonly IMongoCollection<CategoryModel> _categories;
+        private readonly IMemoryCache _cache;
+        private const string cachName = "CategoryData";
+
+        public MongoCategoryData(IDbConnection db, IMemoryCache cache)
         {
-                
+            _cache = cache;
+            _categories = db.CategoryCollection;
         }
+
+        public async Task<List<CategoryModel>> GetAllCategories()
+        {
+            var output = _cache.Get<List<CategoryModel>>(cachName);
+            if (output is null)
+            {
+                var results = await _categories.FindAsync(_ => true);
+                output = results.ToList();
+                _cache.Set(cachName, output, TimeSpan.FromDays(value: 1));
+            }
+            return output;
+        }
+        public Task CreateCategory(CategoryModel category)
+        {
+            return _categories.InsertOneAsync(category);
+        }
+
     }
 }
